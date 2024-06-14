338fcefb3b226de3196bdb0b4fad3321c8a17e23
eidanrdev04 <eidanrdev04@gmail.com>
2024-06-14 03:06:46 -0400

    crud municipio

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { LocalidadModule } from './localidad/localidad.module';
 import { ClienteModule } from './cliente/cliente.module';
 import { DireccionModule } from './direccion/direccion.module';
 import { EstadoModule } from './estado/estado.module';
+import { MunicipioModule } from './municipio/municipio.module';
 
 @Module({
-  imports: [LocalidadModule, ClienteModule, DireccionModule, EstadoModule],
+  imports: [LocalidadModule, ClienteModule, DireccionModule, EstadoModule, MunicipioModule],
   controllers: [AppController],
   providers: [AppService],
 })
